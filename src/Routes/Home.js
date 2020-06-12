0d37744200780b689a7a33350849584405517b88
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-12 16:05:17 +0900

    Fix github folder name error

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
-import Menu from "../A/Menu";
+import Menu from "../Components/Menu";
 import { useMove } from "react-use-gesture";
-import Content from "../A/Content";
+import Content from "../Components/Content";
 
 const Wrapper = styled.div`
   overflow-x: hidden;
