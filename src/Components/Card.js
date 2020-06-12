333e0e430d91d091c7e9dea5aa559367782bb9f2
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-12 17:17:22 +0900

    Update Card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import icon_plus from "../Assets/Icons/icon_plus.png";
 
 const Wrapper = styled.div`
   width: ${(props) => props.cardWidth}px;
   height: ${(props) => props.cardWidth * 0.75}px;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-end;
   font-size: 20px;
   overflow: hidden;
 `;
@@ -24,6 +25,29 @@ const OpacityDiv = styled.div`
   transition: z-index 0.4s, transform 0.4s, opacity 0.4s;
 `;
 
+const IconBox = styled.div`
+  position: absolute;
+  z-index: ${(props) => (props.focused ? 50 : 0)};
+  opacity: ${(props) => (props.focused ? 1 : 0)};
+  background-color: #ff6833;
+  width: 34px;
+  height: 34px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(${(props) => (props.focused ? "-53px" : "0px")});
+  border-radius: 100%;
+  transition: z-index 0.4s, transform 0.4s, opacity 0.4s;
+`;
+
+const IconPlus = styled.div`
+  background-image: url(${(props) => props.url});
+  background-size: contain;
+  background-position: center;
+  width: 20px;
+  height: 20px;
+`;
+
 const ImageDiv = styled.div`
   position: absolute;
   width: ${(props) => props.cardWidth}px;
@@ -33,7 +57,7 @@ const ImageDiv = styled.div`
   align-items: flex-end;
   background-color: ${(props) => props.bgColor};
   transform: translateY(${(props) => (props.focused ? "-70px" : "0px")});
-  transition: z-index 0.4s, transform 0.4s, opacity 0.4s;
+  transition: z-index 0.4s, transform 0.4s;
 `;
 
 const TextDiv = styled.div`
@@ -42,20 +66,49 @@ const TextDiv = styled.div`
   height: ${(props) => props.cardWidth * 0.75}px;
   z-index: ${(props) => (props.focused ? 50 : 0)};
   transform: translateY(${(props) => (props.focused ? "70px" : "0px")});
-  background-color: black;
+  background-color: #1f1f1f;
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  color: white;
   transition: z-index 0.4s, transform 0.4s;
 `;
 
 const TextInner = styled.div`
   width: 100%;
   height: 140px;
+  opacity: ${(props) => (props.focused ? 1 : 0)};
   display: flex;
   justify-content: center;
+  align-items: flex-start;
+  transition: opacity 0.4s;
+`;
+
+const TextBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+`;
+
+const TitleText = styled.div`
+  width: 90%;
+  height: 20px;
+  padding: 5px;
+  font-size: 18px;
+  line-height: 20px;
+  color: white;
+  text-align: center;
+`;
+
+const StringText = styled.div`
+  width: 90%;
+  height: 20px;
+  font-size: 12px;
+  line-height: 20px;
+  color: #737373;
+  text-align: center;
 `;
 
 const Card = ({ rowMaxCount, data, cardWidth }) => {
@@ -79,7 +132,12 @@ const Card = ({ rowMaxCount, data, cardWidth }) => {
           rowMaxCount={rowMaxCount}
           focused={focused}
         >
-          <TextInner>{data.id}</TextInner>
+          <TextInner focused={focused}>
+            <TextBox>
+              <TitleText>{data.titleText}</TitleText>
+              <StringText>{data.stringText}</StringText>
+            </TextBox>
+          </TextInner>
         </TextDiv>
         <ImageDiv
           cardWidth={cardWidth}
@@ -87,6 +145,9 @@ const Card = ({ rowMaxCount, data, cardWidth }) => {
           bgColor={data.bgColor}
           focused={focused}
         ></ImageDiv>
+        <IconBox focused={focused}>
+          <IconPlus url={icon_plus} />
+        </IconBox>
         <OpacityDiv
           cardWidth={cardWidth}
           rowMaxCount={rowMaxCount}
