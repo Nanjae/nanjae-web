24e20a7b5497f72fe50e8e575cc5b6d510491cd5
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-06-12 16:07:35 +0900

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { DefaultRoute } from "./Routes";
-import GlobalStyles from "../styles/GlobalStyles";
+import GlobalStyles from "../Styles/GlobalStyles";
 import { HashRouter as Router } from "react-router-dom";
 
 const Wrapper = styled.div`
